82c02df8207c82b007515b83319f0276651a82ec
Nomaan Ahgharian <get@nomaan.me>
2017-06-30 03:33:46 +0000

    added toggle on and off -- some clean up

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require('express');
 const app = express();
 var rpio = require('rpio');
@@ -5,38 +6,63 @@ var rpio = require('rpio');
 rpio.init({ mapping: 'physical' });
 
 app.get('/', (req, res) => {
-    res.send('Yo');
+    res.send('Yo, what the hell are you going here?');
 })
 
-const relayPins = [3, 5, 7, 9, 11, 13, 15, 19, 21];
+const relayPins = [3, 5, 7, 11, 13, 15, 19, 21];
 
 
 app.get('/api/relay/:pin/status', (req, res) => {
-    const pinNumber = parseInt(req.params.pin, 10);
-    res.send({ status: rpio.read(pinNumber) });
+    const status = getStatus(req.params.pin);
+    res.send({ status: status === 0 ? 'on' : 'off' });
 });
 
 app.get('/api/relay/:pin/toggle', (req, res) => {
-    const pin = parseInt(req.params.pin, 10);
-    rpio.open(pin, rpio.OUTPUT)
-    if (rpio.read(pin) === 1) {
-        rpio.write(pin, rpio.LOW);
-    } else {
-        rpio.write(pin, rpio.HIGH);
-    }
-});
-
-app.get('/api/relay/toggle-all', (req, res) => {
-    relayPins.map(pin => {
-        if (rpio.read(pin) === 1) {
-            rpio.write(pin, rpio.LOW);
-        } else {
-            rpio.write(pin, rpio.HIGH);
+    const pin = req.params.pin;
+    openPin(pin);
+    isOn(pin) ? turnOff(pin): turnOff(pin);
+    res.send({ 
+        pin: {
+            number: pin,
+            isOn: isOn(pin) 
         }
     });
+});
+
+app.get('/api/relay/all/on', (req, res) => {
+    console.log('everything is going up!');
+
+    if(!relayPins.every(isOn)) {
+        relayPins.forEach(pin => rpio.write(pin, rpio.LOW));
+        res.send({ isAllOn: true });
+    } else {
+        res.send({ isAllOn: true });
+        relayPins.forEach(pin =>{
+            if(!isOn(pin)) turnOn(pin);
+        });
+        res.send({ isAllOn: true });
+    }
 })
 
+app.get('/api/relay/all/off', (req, res) => {
+    console.log('world is shutting down');
 
+    if(relayPins.every(isOn)) {
+        relayPins.forEach(pin => turnOff(pin));
+        res.send({ isAllOn: false });
+    } else {
+        relayPins.forEach(pin =>{
+            if(isOn(pin)) turnOff(pin);
+        });
+        res.send({ isAllOn: false });
+    }
+})
+
+const getStatus = (pin) => rpio.read(pin);
+const isOn = (pin) => rpio.read(pin) === 0;
+const turnOff = (pin) => rpio.write(pin, rpio.HIGH);
+const turnOn = (pin) => rpio.write(pin, rpio.LOW);
+const openPin = (pin) => rpio.open(pin, rpio.OUTPUT);
 
 app.listen(3000, () => {
     console.log('Running...')
