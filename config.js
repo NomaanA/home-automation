681247ab55848eff0bae1eac0c20c8797e2d2718
Nomaan Ahgharian <get@nomaan.me>
2017-07-09 05:28:10 +0000

    added a config file

---
+++ config.js
@@ -0,0 +1,15 @@
+const relayPins = [16, 18, 22, 11, 13, 15, 19, 21];
+
+const config = {
+    relays: {
+        1: relayPins[0],
+        2: relayPins[1],
+        3: relayPins[2],
+        4: relayPins[3],
+        5: relayPins[4],
+        6: relayPins[5],
+        7: relayPins[6],
+        8: relayPins[7],
+    }
+}
+module.exports = config;
