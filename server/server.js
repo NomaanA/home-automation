cc4f21bc3972bfff40fabc4cba3d21e55370c9d8
Nomaan Ahgharian <get@nomaan.me>
2017-07-09 05:31:36 +0000

    Updated Server with an API route

--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
 'use strict'
 const express = require('express')
 const server = module.exports = express()
-const path = require('path')
-const api = require('./api')
-const port = process.env.PORT || 3000
 
-var rpio = require('rpio')
-rpio.init({ mapping: 'physical' })
+const path = require('path');
+const port = process.env.PORT || 3000
+const router = express.Router();
+
+// different routes 
+const api = require('./routes/api/');
 
 server.set('view engine', 'html')
-    // server.use(express.static('client/assets'))
-    // server.set('views', './views')
+
+server.use('/api', api);
+server.use((req, res) => {
+    res.status(404).send({ url: req.originalUrl + ' not found' })
+})
 
 server.get('/', (req, res) => {
     server.use(express.static('client/assets'))
     res.sendFile(path.join(__dirname + '/../client/index.html'))
 })
-
-server.use(function(req, res) {
-        res.status(404).send({ url: req.originalUrl + ' not found' })
-    })
-    // server.use('/api', express.static(path.join(__dirname + '/api')))
 
 server.listen(port, () => {
     console.log(`Running on port ${port}...`)
