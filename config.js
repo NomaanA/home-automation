c4a097caea5d32e6de49f64c8aad6be4390ddcb8
Nomaan Ahgharian <get@nomaan.me>
2017-09-27 02:21:32 +0000

    Update config with in use households

--- config.js
+++ config.js
@@ -4,40 +4,40 @@ const config = {
     litOnSunSet: true,
     longitude: '41.815101',
     latitude: '-88.261404',
-    relays: {
-        1: {
+    relays: [
+        {
             pin: relayPins[0],
             name: ''
         },
-        2: {
+        {
             pin: relayPins[1],
             name: ''
         },
-        3: {
+        {
             pin: relayPins[2],
-            name: ' '
-        },
-        4: {
-            pin: relayPins[3],
-            name: 'wax warmer'
-        },
-        5: {
-            pin: relayPins[4],
             name: ''
         },
-        6: {
+        {
+            pin: relayPins[3],
+            name: ''
+        },
+        {
+            pin: relayPins[4],
+            name: 'TV LED'
+        },
+        {
             pin: relayPins[5],
             name: ''
         },
-        7: {
+        {
             pin: relayPins[6],
-            name: 'floor lamp'
+            name: 'Wax Warmer'
         },
-        8: {
+        {
             pin: relayPins[7],
-            name: 'TV led'
-        },
-    },
+            name: 'Floor Lamp'
+        }
+    ],
     relayPins
-}
+};
 module.exports = config;
